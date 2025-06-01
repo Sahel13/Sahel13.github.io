65763b66953f4a9a48f64862a6093c876f3924ae
Sahel Iqbal <sahel13miqbal@proton.me>
2025-06-01 20:00:13 +0300

    Add bibtex citation support

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Sahel13.github.io
+# My Personal Website
 
 Source code for my personal website. The website is generated using
 [Hakyll](https://jaspervdj.be/hakyll/index.html), a static site generator
@@ -9,7 +9,3 @@ by the following sources:
 - [Ayush Singh's blog](https://github.com/11DE784A/11DE784A.github.io).
 - [Tufte CSS](https://edwardtufte.github.io/tufte-css/).
 - [Gwern.net](https://gwern.net/).
-
-## Features
-
-- [KaTex](https://katex.org/) for rendering math equations. The implementation is taken from [Tony Zorman's blog post](https://tony-zorman.com/posts/katex-with-hakyll.html) on the subject. It uses [deno](https://deno.com/) to run KaTex.
--- site.hs
+++ site.hs
@@ -1,19 +1,20 @@
 {-# LANGUAGE BangPatterns        #-}
 {-# LANGUAGE BlockArguments      #-}
-{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE ViewPatterns        #-}
-import Control.Monad (forM_)
+import Control.Monad ((<=<), forM_)
 import Hakyll
 import Data.List (foldl')
-import Data.Text    qualified as T
+import qualified Data.Text as T
 import Data.Text (Text)
-import Data.Text.IO qualified as T
+import qualified Data.Text.IO as T
 import GHC.IO.Handle (BufferMode (NoBuffering), Handle, hSetBuffering)
 import System.Process (runInteractiveCommand)
 import Text.Pandoc.Definition (Block (..), Inline (..), MathType (..), Pandoc)
+import Text.Pandoc.Builder (setMeta)
 import Text.Pandoc.Walk (walk, walkM)
 
 -- GitHub Pages expects the website to be in `/docs`.
@@ -22,44 +23,57 @@ myConfig = defaultConfiguration { destinationDirectory = "docs" }
 
 main :: IO ()
 main = hakyllWith myConfig $ do
-    forM_ ["images/*", "files/*"] $ \f -> match f $ do
-        route   idRoute
-        compile copyFileCompiler
+  forM_ ["images/*", "files/*"] $ \f -> match f $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "styles/main.scss" $ do
-        route $ setExtension "css"
-        compile compressScssCompiler
+  match "styles/main.scss" $ do
+    route $ setExtension "css"
+    compile compressScssCompiler
 
-    forM_ ["styles/*.css", "styles/fonts/*"] $ \f -> match f $ do
-        route   idRoute
-        compile copyFileCompiler
+  forM_ ["styles/*.css", "styles/fonts/*"] $ \f -> match f $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "index.md" $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match "index.md" $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
 
-    match "templates/*" $ compile templateBodyCompiler
+  -- Citations
+  match "bib/style.csl"        $ compile cslCompiler
+  match "bib/bibliography.bib" $ compile biblioCompiler
+
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ myPandocCompiler
+      >>= loadAndApplyTemplate "templates/post.html"    postCtx
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
+
+  match "templates/*" $ compile templateBodyCompiler
 
 myPandocCompiler :: Compiler (Item String)
 myPandocCompiler =
-    pandocCompilerWithTransformM
-        defaultHakyllReaderOptions
-        defaultHakyllWriterOptions
-        hlKaTeX
+  pandocItemCompilerWithTransformM
+    defaultHakyllReaderOptions
+    defaultHakyllWriterOptions
+    ( traverse hlKaTeX
+     <=< processBib
+    )
 
 -- Compiler for SCSS.
 -- Requires the `sass` command line tool.
 -- Source : https://github.com/jjduhamel/blog/blob/master/site.hs
 compressScssCompiler :: Compiler (Item String)
 compressScssCompiler = do
-    fmap (fmap compressCss) $
-        getResourceString
-        >>= withItemBody (unixFilter "sass" [ "--stdin"
-                                            , "--style", "compressed"
-                                            , "--load-path", "./styles"
-                                            ])
+  fmap (fmap compressCss) $
+    getResourceString
+    >>= withItemBody (unixFilter "sass" [ "--stdin"
+                                        , "--style", "compressed"
+                                        , "--load-path", "./styles"
+                                        ])
 
 -- KaTeX compiler for Pandoc from https://tony-zorman.com/posts/katex-with-hakyll.html
 hlKaTeX :: Pandoc -> Compiler Pandoc
@@ -104,3 +118,16 @@ hlKaTeX pandoc = recompilingUnsafeCompiler do
     , ("\\kVect" , "\\mathsf{Vect}_{\\mathtt{k}}")
     ]
 
+processBib :: Item Pandoc -> Compiler (Item Pandoc)
+processBib pandoc = do
+  csl <- load @CSL    "bib/style.csl"
+  bib <- load @Biblio "bib/bibliography.bib"
+  -- Link citations.
+  p <- withItemBody (pure . setMeta "link-citations" True) pandoc
+  fmap insertRefHeading <$> processPandocBiblio csl bib p
+    where
+      -- Insert a heading for the citations.
+      insertRefHeading :: Pandoc -> Pandoc
+      insertRefHeading = walk $ concatMap \case
+        d@(Div ("refs", _, _) _) -> [Header 2 ("references", [], []) [Str "References"], d]
+        block                    -> [block]
